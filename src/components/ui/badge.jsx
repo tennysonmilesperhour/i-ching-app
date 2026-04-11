16786f2b9addbf26dd11bfdef5fce4d5ce41c9a0
Tennyson <tennysontaggart@gmail.com>
2026-04-10 22:47:54 -0600

    Create badge.jsx

---
+++ src/components/ui/badge.jsx
@@ -0,0 +1,32 @@
+import * as React from "react"
+import { cva } from "class-variance-authority";
+
+import { cn } from "@/lib/utils"
+
+const badgeVariants = cva(
+  "inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-background transition-colors [&:hover]:bg-primary/80
+    {
+      default: "",
+      secondary:
+        "border-transsparent bg-secondary text-secondary-foreground hover:bg-secondary/80",
+        destructive:
+          "border-transparent bg-destructive text-destructive-foreground shadow hover:bg-destructive/80",
+        outline: "text-foreground",
+      },
+    },
+    defaultVariants: {
+          defaultVariants: {
+      variant: "default",
+    },
+  }
+)
+
+function Badge({
+  className,
+  variant,
+  ...props
+}) {
+  return (<div className={cn(badgeVariants({ variant }), className)} {...props} />);
+}
+
+export { Badge, badgeVariants }
